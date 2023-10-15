c1420c235d84c5589f62957f18bcaad5457f2636
Jiao-Yiyang <1391079660@qq.com>
2023-10-15 11:29:13 +0800

    test

--- project1/src/main/java/app/MyAnnotationApp.java
+++ project1/src/main/java/app/MyAnnotationApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyAnnotationApp {
     public static void main(String[] args) {
-
+        //test
         SpringApplication.run(MyAnnotationApp.class, args);
 
     }
